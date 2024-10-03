234b09532a6a62211e1fdef54c95f92cbc47f689
salmanfxrsi <salmanxprivate@gmail.com>
2024-10-03 16:50:25 +0600

    Worked On loadCategoriesVideo Located In video.js!

--- scripts/video.js
+++ scripts/video.js
@@ -8,11 +8,20 @@ const categoriesData = async() => {
 const getCategories = (categories) => {
     const categoriesSection = document.getElementById('categories')
     categories.forEach(category => {
-        const categoryButton = document.createElement('button');
-        categoryButton.classList = 'py-2 px-5 bg-[#25252526] bg-opacity-15 rounded-[4px]'
-        categoryButton.innerText = category.category;
-        categoriesSection.appendChild(categoryButton)
+        const categoryButtonContainer = document.createElement('div');
+        categoryButtonContainer.innerHTML = `
+        <button onclick='loadCategoriesVideo(${category.category_id})' class="btn py-2 px-5 bg-[#25252526] bg-opacity-15 rounded-[4px]">
+        ${category.category}
+        </button>
+        `
+        categoriesSection.appendChild(categoryButtonContainer)
     })
+}
+
+const loadCategoriesVideo = async(id) => {
+    const response = await fetch (`https://openapi.programming-hero.com/api/phero-tube/category/${id}`);
+    const data = await response.json();
+    getVideos(data.category)
 }
 
 categoriesData()
@@ -26,6 +35,7 @@ const videosData = async() => {
 
 const getVideos = (videos) => {
     const videosSection = document.getElementById('videos')
+    videosSection.innerHTML = ""
     videos.forEach(video => {
         const videosCard = document.createElement('div');
         videosCard.innerHTML = `
@@ -36,7 +46,10 @@ const getVideos = (videos) => {
             <div><img class="h-10 w-10 rounded-full" src=${video.authors[0].profile_picture} alt=""></div>
             <div>
                 <p class="font-bold text-base text-[#171717]">${video.title}</p>
-                <p class="text-sm text-[#171717B3] text-opacity-70 mt-1">${video.authors[0].profile_name}</p>
+                <div class="flex gap-1 items-center mt-1">
+                   <p class="text-sm text-[#171717B3] text-opacity-70">${video.authors[0].profile_name}</p>
+                   ${video.authors[0].verified === true ? '<img class="w-4 h-4" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png">' : ""}
+                </div>
                 <p class="text-sm text-[#171717B3] text-opacity-70 mt-1">${video.others.views}</p>
             </div>
         </div>
@@ -46,4 +59,5 @@ const getVideos = (videos) => {
 }
 
 videosData()
+
 
